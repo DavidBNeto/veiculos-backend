2bb53fef49a229c9b77ff7782388427a25f8ad39
MagicManatee <arthur.ibarra@edu.pucrs.br>
2023-05-19 16:56:29 -0300

    refactor: Default date format.

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 
 
-# This function returns the current date in the format 'YYYY-MM-DD--HH:MM:SS'
+DEFAULT_DATE_FORMAT = "%Y-%m-%d--%H:%M:%S"
+
+
+# This function returns the current date in the format DEFAULT_DATE_FORMAT.
 def current_date():
-    return datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
+    return datetime.now().strftime(DEFAULT_DATE_FORMAT)
 
 
 # This function tells if a date in string came after another date in string.
-# The dates must be in the format 'YYYY-MM-DD--HH:MM:SS'
+# The dates must be in the format DEFAULT_DATE_FORMAT.
 def is_date_after(date_1: str, date_2: str):
-    return datetime.strptime(date_1, "%Y-%m-%d--%H:%M:%S") > datetime.strptime(date_2, "%Y-%m-%d--%H:%M:%S")
+    return datetime.strptime(date_1, DEFAULT_DATE_FORMAT) > datetime.strptime(date_2, DEFAULT_DATE_FORMAT)
